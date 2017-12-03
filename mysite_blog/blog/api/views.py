83c8d93a0a6cae55b1536d9eb4eb5f955d89429e
samghadri <sam.ghadri@gmail.com>
2017-12-03 17:51:46 +0000

    permissionAPi

--- mysite_blog/blog/api/views.py
+++ mysite_blog/blog/api/views.py
@@ -2,10 +2,15 @@ from rest_framework.generics import (ListAPIView, RetrieveAPIView,
                                     UpdateAPIView, DestroyAPIView, CreateAPIView)
 from blog.models import Post
 from .serializers import PostSerializer, PostDetailSerializer, PostCreateUpdateSerializer
+from rest_framework.permissions import (AllowAny,
+                                        IsAuthenticated,
+                                        IsAdminUser,
+                                        IsAuthenticatedOrReadOnly)
 
 class PostApi(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
 
 
 class PostDetailApi(RetrieveAPIView):
@@ -29,3 +34,4 @@ class PostUpdateApi(UpdateAPIView):
 class PostCreateApi(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
