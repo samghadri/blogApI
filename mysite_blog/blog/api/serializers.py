ab83d5690715b11fdee9ea6f028b958246105314
samghadri <sam.ghadri@gmail.com>
2017-12-03 22:07:02 +0000

    HyplinkObjectandDeleteUrl

--- mysite_blog/blog/api/serializers.py
+++ mysite_blog/blog/api/serializers.py
@@ -1,17 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
 from blog.models import Post
 
 
 class PostSerializer(ModelSerializer):
+
+    url = HyperlinkedIdentityField(
+        view_name='blog-api:detail',
+        lookup_field='slug'
+        )
+    delete_url = HyperlinkedIdentityField(
+        view_name='blog-api:delete',
+        lookup_field='slug'
+        )
     class Meta:
         model = Post
-        fields =['author','title','text','slug']
+        fields =['author','url','title','text','delete_url']
 
 
 class PostDetailSerializer(ModelSerializer):
     class Meta:
         model = Post
-        fields =['author', 'title', 'text', 'slug']
+        fields =['author', 'title', 'text','created_date', 'slug']
 
 
 class PostCreateUpdateSerializer(ModelSerializer):
